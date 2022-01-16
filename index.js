8bb1a29d63edfbb37470a92647905a66a1668a27
Corban <sonnemanc@gmail.com>
2022-01-16 15:38:29 -0800

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const { createVirtualConsole } = require("jsdom")
+
+
+function writeCards( names, event ) {
+    let cards = []
+    for ( let i = 0; i < names.length; i++ ) {
+        cards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    return cards
+}
+
+function countDown( n ) {
+    while (n >= 0) {
+        console.log(n--)
+    }
+}
+
